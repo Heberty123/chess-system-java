82a3dc11a496526173dc06d1857a3041f09f5338
Heberty Guelfi do Prado <hebertyprado22005@gmail.com>
2022-06-04 00:57:00 +0100

    Firts class: position

---
+++ src/Program.java
@@ -0,0 +1,10 @@
+import boardgame.Position;
+
+public class Program {
+
+    public static void main(String []argc){
+
+        Position pos = new Position(2, 5);
+        System.out.println(pos);
+    }
+}
